6e89ee5cf38a799375df4b0a8cbe0c008a91ddd8
igormayachenkov <igormayachenkov@gmail.com>
2023-06-09 00:05:15 +0300

    AppBar: Row instead of TopAppBar

--- list/src/main/java/ru/igormayachenkov/list/ui/AppBar.kt
+++ list/src/main/java/ru/igormayachenkov/list/ui/AppBar.kt
@@ -1,17 +1,18 @@
 package ru.igormayachenkov.list.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import ru.igormayachenkov.list.ui.theme.ListTheme
 
@@ -29,38 +30,40 @@ fun AppBar(
     var showMenu by remember { mutableStateOf(false) }
     fun onMenuItem(handler:()->Unit){showMenu=false; handler()}
 
-    var navIcon : @Composable (() -> Unit)? = null
-    if (!isRoot){
-        navIcon = @Composable {
+    Row(
+        Modifier
+            .background(color = MaterialTheme.colors.primary)
+            .defaultMinSize(minHeight = 48.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        // Back button (Navigation icon)
+        if (isRoot)
+            Spacer(modifier = Modifier.width(8.dp))
+        else
             IconButton(onClick = onBack) {
                 Icon(Icons.Default.ArrowBack,"")
             }
-        }
-    }
 
-    TopAppBar(
-        backgroundColor = MaterialTheme.colors.primary,
-        navigationIcon = navIcon,
-        title = {
-            // Title
-            Text(text = title, color = MaterialTheme.colors.onPrimary,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clickable(onClick = { if (!isRoot) onEdit() }))
-        },
-        actions = {
-            // Create Button
-            if(showOnCreate) IconButton(onClick = onCreate) {
-                Icon(Icons.Default.AddCircle,"",
-                    tint = MaterialTheme.colors.onPrimary)
-            }
-            // Menu Button
-            IconButton(onClick = { showMenu=true }) {
-                Icon(Icons.Default.MoreVert, contentDescription = "Localized description")
-            }
+
+        // Title
+        Text(text = title, color = MaterialTheme.colors.onPrimary,
+            modifier = Modifier
+                .weight(1F)
+                .clickable(onClick = { if (!isRoot) onEdit() }))
+
+        // Add (Create) Button
+        if(showOnCreate) IconButton(onClick = onCreate) {
+            Icon(Icons.Default.AddCircle,"",
+                tint = MaterialTheme.colors.onPrimary)
+        }
+
+        // Menu Button
+        IconButton(onClick = { showMenu=true }) {
+            Icon(Icons.Default.MoreVert, contentDescription = "Localized description")
             // Menu
             DropdownMenu(
                 expanded = showMenu,
+                offset = DpOffset.Zero,
                 onDismissRequest = { showMenu=false }
             ) {
                 // - Edit
@@ -86,7 +89,7 @@ fun AppBar(
                 }
             }
         }
-    )
+    }
 }
 //--------------------------------------------------------------------------------------------------
 // PREVIEW
